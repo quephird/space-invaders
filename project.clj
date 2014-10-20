6744aea82e9b70bbe512f2f22f958e1a77677623
danielle kefford <danielle_kefford@hotmail.com>
2014-10-20 11:14:09 -0400

    First commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject space-invaders "1.0.0-SNAPSHOT"
+  :description "Space Invaders game inspired by Mary Rose Cook"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [quil "2.2.2"]
+                 [ddf.minim "2.2.0"]])
